a04fd9042cd1c7baf399914f18eb56996b9f7d0f
cmolinari99 <cami.molinari@gmail.com>
2026-05-01 18:42:15 -0300

    hdr

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { SpotifyLogo } from "../components/spotify-logo/spotify-logo";
+import { NgIcon, provideIcons } from '@ng-icons/core';
+
+import { solarHomeBold } from '@ng-icons/solar-icons/bold';
+import { solarRoundArrowDownLinear,
+         solarBellLinear,
+         solarMagniferLinear,
+         solarLibraryLinear,
+         solarUsersGroupTwoRoundedLinear,
+         solarAddCircleLinear,
+         solarMaximizeLinear
+                     } from '@ng-icons/solar-icons/linear';
+
+
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [SpotifyLogo, NgIcon],
+  viewProviders: [
+    provideIcons({ 
+      solarHomeBold, 
+      solarRoundArrowDownLinear,
+      solarBellLinear,
+      solarMagniferLinear,
+      solarLibraryLinear,
+      solarUsersGroupTwoRoundedLinear,
+      solarAddCircleLinear,
+      solarMaximizeLinear }),
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
