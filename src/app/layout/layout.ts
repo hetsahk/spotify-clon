7b60ff346e6c05b074393b3a95f2fac232351c31
cmolinari99 <cami.molinari@gmail.com>
2026-05-03 20:54:03 -0300

    main

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -3,7 +3,10 @@ import { SpotifyLogo } from "../components/spotify-logo/spotify-logo";
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { Card } from '../components/card/card';
 
-import { solarHomeBold } from '@ng-icons/solar-icons/bold';
+import { solarHomeBold,
+         solarCatBold,
+         solarTextCircleBold
+                     } from '@ng-icons/solar-icons/bold';
 import { solarRoundArrowDownLinear,
          solarBellLinear,
          solarMagniferLinear,
@@ -11,7 +14,8 @@ import { solarRoundArrowDownLinear,
          solarUsersGroupTwoRoundedLinear,
          solarAddCircleLinear,
          solarMaximizeLinear,
-         solarList1Linear
+         solarList1Linear,
+         solarCameraMinimalisticLinear
                      } from '@ng-icons/solar-icons/linear';
 
 type CardData = {
@@ -35,7 +39,10 @@ type CardData = {
       solarUsersGroupTwoRoundedLinear,
       solarAddCircleLinear,
       solarMaximizeLinear,
-      solarList1Linear }),
+      solarList1Linear,
+      solarCameraMinimalisticLinear,
+      solarCatBold,
+      solarTextCircleBold }),
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
@@ -102,5 +109,69 @@ export class Layout {
       subtitle: 'Playlist • Spotify',
       imageUrl: 'https://picsum.photos/seed/radar/100',
     },
+  ];
+
+  readonly squareCards: CardData[] = [
+    {
+      title: 'Maria Becerra',
+      subtitle: 'With TINI, TULI...',
+      imageUrl: 'https://picsum.photos/seed/maria/300',
+    },
+    {
+      title: 'Top 50 Argentina',
+      subtitle: 'Los hits más escuchados',
+      imageUrl: 'https://picsum.photos/seed/top50ar/300',
+    },
+    {
+      title: 'Cumbia 420',
+      subtitle: 'L-Gante, Damas Gratis...',
+      imageUrl: 'https://picsum.photos/seed/cumbia/300',
+    },
+    {
+      title: 'Rock Nacional',
+      subtitle: 'Soda, Los Redondos...',
+      imageUrl: 'https://picsum.photos/seed/rocknac/300',
+    },
+    {
+      title: 'Maria Becerra',
+      subtitle: 'With TINI, TULI...',
+      imageUrl: 'https://picsum.photos/seed/maria/300',
+    },
+    {
+      title: 'Top 50 Argentina',
+      subtitle: 'Los hits más escuchados',
+      imageUrl: 'https://picsum.photos/seed/top50ar/300',
+    },
+    {
+      title: 'Cumbia 420',
+      subtitle: 'L-Gante, Damas Gratis...',
+      imageUrl: 'https://picsum.photos/seed/cumbia/300',
+    },
+    {
+      title: 'Maria Becerra',
+      subtitle: 'With TINI, TULI...',
+      imageUrl: 'https://picsum.photos/seed/maria/300',
+    },
+    {
+      title: 'Top 50 Argentina',
+      subtitle: 'Los hits más escuchados',
+      imageUrl: 'https://picsum.photos/seed/top50ar/300',
+    },
+    {
+      title: 'Cumbia 420',
+      subtitle: 'L-Gante, Damas Gratis...',
+      imageUrl: 'https://picsum.photos/seed/cumbia/300',
+    },
+  ];
+
+  readonly circleCards: CardData[] = [
+    { title: 'Bad Bunny', subtitle: 'Artist', imageUrl: 'https://picsum.photos/seed/badbunny/300' },
+    { title: 'Duki', subtitle: 'Artist', imageUrl: 'https://picsum.photos/seed/duki/300' },
+    { title: 'Nathy Peluso', subtitle: 'Artist', imageUrl: 'https://picsum.photos/seed/nathy/300' },
+    { title: 'Rosalia', subtitle: 'Artist', imageUrl: 'https://picsum.photos/seed/rosalia/300' },
+    { title: 'Bad Bunny', subtitle: 'Artist', imageUrl: 'https://picsum.photos/seed/badbunny/300' },
+    { title: 'Duki', subtitle: 'Artist', imageUrl: 'https://picsum.photos/seed/duki/300' },
+    { title: 'Nathy Peluso', subtitle: 'Artist', imageUrl: 'https://picsum.photos/seed/nathy/300' },
+    { title: 'Rosalia', subtitle: 'Artist', imageUrl: 'https://picsum.photos/seed/rosalia/300' },
   ];
 }
